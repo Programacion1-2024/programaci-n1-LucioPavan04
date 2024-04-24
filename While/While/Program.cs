8427aff503e22e489494af21a9c91cdfeff5bbf1
LucioPavan04 <luciomartin02@gmail.com>
2024-04-24 19:45:15 -0300

    commit

---
+++ While/While/Program.cs
@@ -0,0 +1,24 @@
+﻿//El usuario ingresará valores positivos, cuando no tenga más valores ingresará 0
+//(cero).El sistema deberá informar la suma de los ingresados.
+/*
+using System.Drawing;
+
+Console.WriteLine("ingrese los valores positivos deseados, cuando no quiera ingresar mas valores ingrese 0");
+
+double valor = 1;
+while (valor > 0)
+{
+    if (double.TryParse(Console.ReadLine()?.Replace(".", ","), out valor)) {
+        if (valor != 0)
+        {
+            Console.WriteLine("El valor ingresado es: " + valor);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Valor invalido.");
+        valor = 1;
+    }
+}
+*/
+//ejercicio 2 
